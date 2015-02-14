52e99f99796816e92f566d91106d5c665f041a31
Monsterblader <Monsterblader@gmail.com>
2015-02-14 13:04:33 -0800

    (fix) Implement Grunt jshint.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,16 +6,34 @@ module.exports = function (grunt) {
   // show elapsed time at the end
   require('time-grunt')(grunt);
   // load all grunt tasks
+  //al Library - replaces grunt.loadNpmTasks()
   require('load-grunt-tasks')(grunt);
 
   var reloadPort = 35729, files;
 
   grunt.initConfig({
+    //al ? how to set variables in package.json
     pkg: grunt.file.readJSON('package.json'),
+    //al grunt-develop
     develop: {
       server: {
         file: 'app.js'
+        //al , nodeArgs: ['--debug'],
+        //al args: ['appArg1', 'appArg2'],
+        //al env: { NODE_ENV: 'development'}
       }
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        'Gruntfile.js',
+        'public/js/**/*.js',
+        'test/unit/**/*.js',
+          '!test/unit/lib/**/*.js'
+      ]
     },
     watch: {
       options: {
@@ -43,7 +61,9 @@ module.exports = function (grunt) {
           'server/views/*.ejs',
           'server/views/**/*.ejs'
         ],
-        options: { livereload: reloadPort }
+        options: {
+          livereload: reloadPort
+        }
       }
     }
   });
@@ -66,8 +86,39 @@ module.exports = function (grunt) {
     }, 500);
   });
 
-  grunt.registerTask('default', [
+  grunt.registerTask('createDefaultTemplate', function () {
+    grunt.file.write('.tmp/scripts/templates.js', 'this.JST = this.JST || {};');
+  });
+
+  grunt.registerTask('test', function (isConnected) {
+    isConnected = Boolean(isConnected);
+    var testTasks = [
+      'clean:server',
+      'clean:results',
+      'createDefaultTemplate',
+      'jst',
+      'connect:test',
+      'karma',
+      'casperjs'
+    ];
+
+    if(!isConnected) {
+      return grunt.task.run(testTasks);
+    } else {
+      // already connected so not going to connect again, remove the connect:test task
+      testTasks.splice(testTasks.indexOf('connect:test'), 1);
+      return grunt.task.run(testTasks);
+    }
+  });
+
+  grunt.registerTask('defaultOld', [
     'develop',
     'watch'
+  ]);
+
+  grunt.registerTask('default', [
+    'jshint',
+    'test',
+    //'build'
   ]);
 };
