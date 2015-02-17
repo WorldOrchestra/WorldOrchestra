dff78e2dd85104b6b63a1ccb73f32db912a04266
Monsterblader <Monsterblader@gmail.com>
2015-02-16 16:30:21 -0800

    Final commit.

--- test/unit/spec.js
+++ test/unit/spec.js
@@ -1,12 +1,66 @@
 describe('WorldOrchestra framework', function() {
-  describe('Track model', function() {
-    it('should contain a track model', function() {
-      expect(typeof WO.Track).to.be('function');
+  describe('The main application', function() {
+    it('should contain a container named WO', function() {
+      expect(typeof WO).to.be('object');
+    });
+    it('should have a master model', function() {
+      expect(WO.WOModel).not.to.be(undefined);
+    });
+    it('should have a master view', function() {
+      expect(WO.WOView).not.to.be(undefined);
+    });
+  });
+
+  describe('The main model', function() {
+    it('should do something', function() {
+      expect(false).to.equal(true);
+    });
+  });
+
+  describe('The main view', function() {
+    it('should have a song view', function() {
+      expect(false).to.equal(true);
+    });
+    it('should have a transport view', function() {
+      expect(false).to.equal(true);
+    });
+    it('should have a userInput view', function() {
+      expect(false).to.equal(true);
+    });
+  });
+
+  describe('The userInput view', function() {
+    it('should exist', function() {
+      expect(false).to.equal(true);
+    });
+    it('should display a instrument', function() {
+      expect(false).to.equal(true);
+    });
+    it('should display toggle buttons for instrument', function() {
+      expect(false).to.equal(true);
+    });
+  });
+
+  describe('The transport view', function() {
+    it('should have a forward button', function() {
+      expect(false).to.equal(true);
+    });
+    it('should have a back button', function() {
+      expect(false).to.equal(true);
+    });
+    it('should have a play button', function() {
+      expect(false).to.equal(true);
+    });
+    it('should have a stop button', function() {
+      expect(false).to.equal(true);
+    });
+    it('should have a record button', function() {
+      expect(false).to.equal(true);
     });
   });
 
   describe('Song collection', function() {
-    it('should contain a song collection', function() {
+    it('should be a Backbone collection', function() {
       expect(typeof WO.Song).to.be('function');
     });
     it('should be able to add a track', function() {
@@ -16,15 +70,9 @@ describe('WorldOrchestra framework', function() {
     });
   });
 
-  describe('The main application', function() {
-    it('should contain a container named WO', function() {
-      expect(typeof WO).to.be('object');
-    });
-    it('should have a master model', function() {
-        expect(WO.WOModel).not.to.be(undefined);
-    });
-    it('should have a master view', function() {
-      expect(WO.WOView).not.to.be(undefined);
+  describe('Track model', function() {
+    it('should be a Backbone model', function() {
+      expect(typeof WO.Track).to.be('function');
     });
   });
 });
