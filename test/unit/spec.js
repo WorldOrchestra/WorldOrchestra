f796a24b8200414237e9ee5aa1b45f42c5196b14
David Fedele <dave.fedele@gmail.com>
2015-02-16 16:37:45 -0800

    add testing framework

---
+++ test/unit/spec.js
@@ -0,0 +1,30 @@
+describe('Backbone server', function() {
+  var track;
+  var song;
+
+  describe('Track model', function() {
+    beforeEach(function() {
+      track = new WO.Track();
+    });
+
+    it('should contain a track model', function() {
+      expect(track).to.be.an("object");
+    });
+  });
+
+  describe('Song collection', function() {
+    beforeEach(function() {
+      track = new WO.Track();
+      song = new WO.Song();
+    });
+
+      it('should contain a song collection', function() {
+          expect(song).to.be.an("object");
+      });
+
+    it('should be able to add a track', function() {
+       song.add(new WO.Track());
+      expect(song.at(0) instanceof WO.Track).to.equal(true);
+    });
+  });
+});
---
+++ test/unit/transportSpec.js
@@ -0,0 +1,19 @@
+/* global describe, it, transport */
+
+(function () {
+  'use strict';
+  var notes;
+  describe('transport.js', function () {
+    describe('recording', function(){
+      it("should record played notes", function(){
+        notes = [ ["0:0:0", "C4", 1.00], ["0:0:1", "C4", 0.00] ];
+        expect(false).to.equal(true);
+      });
+    });
+    describe('playback', function(){
+      it("should record played notes", function(){
+        expect(false).to.equal(true);
+      });
+    });    
+  });
+})();
