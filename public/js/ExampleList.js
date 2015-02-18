31c27b1eb30cfc3cd0ed41a336c548a3ff36e0aa
Monsterblader <Monsterblader@gmail.com>
2015-02-17 20:25:11 -0800

    (feat) Add files for userInterfaceView.

---
+++ public/js/ExampleList.js
@@ -0,0 +1,35 @@
+// TODO Remove this if possible.
+var ExampleList = {
+	"Sources" : {
+		"Oscillators" : "oscillator",
+		"Noise" : "noises",
+		"Player" : "player"
+	},
+	"Components" : {
+		"Envelope" : "envelope",
+		"LFO" : "lfo"
+	},
+	"Signal" : {
+		"Signal Math" : "signalMath",
+	},
+	"Effects" : {
+		"AutoPanner" : "autoPanner",
+		"PingPongDelay" : "pingPongDelay",
+		"Buses" : "buses",
+		"Convolver" : "convolver"
+	},
+	"Instruments" : {
+		"MonoSynth" : "monoSynth",
+		"DuoSynth" : "duoSynth",
+		"FMSynth" : "fmSynth",
+	},
+	"Timing" : {
+		"Notes / Scores" : "score",
+		"Transport" : "stepSequencer"
+	},
+	"Apps" : {
+		"JSON Sandbox" : "json",
+		"Dots" : "interaction",
+		"Play Along" : "shiny"
+	},
+};
