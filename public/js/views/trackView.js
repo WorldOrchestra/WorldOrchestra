a45c9f0e8c4035c6bb10b4b5b1ef78a0391788e2
David Fedele <dave.fedele@gmail.com>
2015-02-19 01:09:55 -0800

    add event for volume slider and mute button

--- public/js/views/trackView.js
+++ public/js/views/trackView.js
@@ -1,7 +1,9 @@
 var WO = WO || {};
 WO.TrackView = Backbone.View.extend({
   events: {
-    'click .delete-track-button' : 'deleteTrack'
+    'click .delete-track-button' : 'deleteTrack',
+    'change .track-volume-slider' : 'setTrackVolume',
+    'click .mute-track-button' : 'muteTrack'
   },
 
   template: _.template(
@@ -9,10 +11,10 @@ WO.TrackView = Backbone.View.extend({
       '<div class="track-controls">' +
         '<p class="track-title"> <%= title %> </p>' +
         '<button class="delete-track-button" id="delete"><i class="fa fa-trash-o"></i></button><br>' +
-        '<button class="solo-button" id="solo">S</button>' +
-        '<button class="mute-button" id="mute" >M</button>' +
+        '<button class="solo-track-button" id="solo">S</button>' +
+        '<button class="mute-track-button" id="mute" >M</button>' +
         '&nbsp;<i class="fa fa-volume-down"></i>' +
-        '<input type="range" min="0.00" max="1.00" step="0.01" value="0.75">' +
+        '<input class="track-volume-slider" type="range" min="0.00" max="1.00" step="0.01" value="0.75">' +
         '<i class="fa fa-volume-up"></i>' +
       '</div>' +
       '<div class="track-notes">' +
@@ -29,6 +31,29 @@ WO.TrackView = Backbone.View.extend({
 
   deleteTrack: function(){
     this.model.collection.remove(this.model);
+  },
+
+  setTrackVolume: function(){
+    var volume = this.$el.find('.track-volume-slider').val();
+    this.model.set('volume', volume);
+    this.model.get('instrument').setVolume(volume*20);
+  },
+
+  muteTrack: function(){
+    var volume, color;
+    if( this.model.get('isMuted') ){
+      volume = 0;
+      this.model.set('isMuted', false);
+      color = "rgb(255,255,255)";
+    }else{
+      volume = -50;
+      this.model.set('isMuted', true);
+      color = "rgb(255,0,0)";
+    }
+    console.log(color);
+    this.$el.find('track-mute-button').css('background-color', color);
+    this.model.set('volume', volume);
+    this.model.get('instrument').setVolume(volume);
   }
 
 });
