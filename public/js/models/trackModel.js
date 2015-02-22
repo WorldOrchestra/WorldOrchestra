7d2db0c14a82ddc51a841cc25901974829f864c7
David Fedele <dave.fedele@gmail.com>
2015-02-21 16:09:43 -0800

    initialize instrument for a new track

--- public/js/models/trackModel.js
+++ public/js/models/trackModel.js
@@ -2,12 +2,19 @@ var WO = WO || {};
 WO.Track = Backbone.Model.extend({
   defaults: {
     notes: [],
-    instrument: WO.acousticPiano,
     title: 'Acoustic Piano',
     isMuted: false,
     solo: false,
     octave: 4,
     volume: 0.75
+  },
+
+  initialize : function(){
+    //set default instrument on load
+    WO.instrumentTrack1 = WO.InstrumentFactory( "Acoustic Piano" );
+    WO.instrumentKeyHandler(WO.instrumentTrack1);
+    this.set('instrument', WO.instrumentTrack1);
   }
+
 });
 
