3282eb6f1d17ee0296d2a4dab9469d868fcf0ac4
David Fedele <dave.fedele@gmail.com>
2015-02-23 15:40:13 -0800

    add new track button

--- public/js/views/WOView.js
+++ public/js/views/WOView.js
@@ -1,6 +1,12 @@
 var WO = WO || {};
 WO.WOView = Backbone.View.extend({
 
+  template: _.template(
+    '<div>' +
+      '<button class="add-track-button"><i class="fa fa-plus"></i></button>' +
+    '</div>'
+    ),
+
   initialize: function(params){
     this.songView = new WO.SongView({collection: new WO.Song()});
     this.transportView = new WO.TransportView();
@@ -9,6 +15,7 @@ WO.WOView = Backbone.View.extend({
   },
 
   render: function(){
+    this.$el.append(this.template);
     this.$el.append(this.transportView.$el);
     this.$el.append(this.userInputView.render());
     $('.appBody').append(this.$el);
