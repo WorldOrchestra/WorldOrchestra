eb76f4ef2dbd831e000adfe9f06f607f579c96f7
David Fedele <dave.fedele@gmail.com>
2015-02-24 16:23:11 -0800

    fix track remove and activeTrack on remove

--- public/js/views/songView.js
+++ public/js/views/songView.js
@@ -6,7 +6,7 @@ WO.SongView = Backbone.View.extend({
     this.render();
     WO.vent.on('click:addTrack', this.addTrack, this);
     this.collection.on("add", this.renderTrack, this);
-    this.collection.on("remove", this.render, this);
+    this.collection.on("remove", this.removeTrack, this);
   },
 
   addTrack: function(){
@@ -16,30 +16,34 @@ WO.SongView = Backbone.View.extend({
     $(document).unbind('keydown');
     $(document).unbind('keyup');
     WO.instrumentKeyHandler(newTrack.attributes.instrument);
-
-    // var trackView = new WO.TrackView({model: track});
-    // this.$el.append(trackView.render());
   },
+
   removeTrack: function(){
-
+    var collectionLength = this.collection.length;
+    this.collection.settings.activeTrack = collectionLength ?  this.collection.at(collectionLength - 1) : "";
+    $('.' + this.collection.settings.activeTrack.cid).addClass('active-track');    
+    this.render();
   },
+
   render: function() {
-  // this.$el.children().remove();
+    this.$el.children().remove();
     $('.appBody').prepend(this.el);
     this.collection.forEach(this.renderTrack, this);
     return this;
   },
+
   renderTrack: function(track){
     var trackView = new WO.TrackView({model: track});
     $('#songView').append(trackView.render());
     $(document).ready((function(){
       track.set('mRender', new WO.MidiRender(track.cid+ ' .track-notes'));
+      track.get('mRender').showTrack(track.get('notes'));
     }).bind(this));
     this.setActiveClass();
   },
+
   setActiveClass: function(){
     $('.active-track').removeClass('active-track');
     $('.' + this.collection.settings.activeTrack.cid).addClass('active-track');
   }
-
 });
