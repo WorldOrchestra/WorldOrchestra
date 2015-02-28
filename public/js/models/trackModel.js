a6cab6aebf67406f1a1b8b0495023d7e68992d67
domenvajevec <domenvajevec@gmail.com>
2015-02-28 14:34:28 -0800

    refactor changeInstrument function to work with all instrument types

--- public/js/models/trackModel.js
+++ public/js/models/trackModel.js
@@ -20,14 +20,42 @@ WO.Track = Backbone.Model.extend({
 
   changeInstrument: function(instrumentName){
     WO.appView.unbindKeys();
-    this.set('instrument', WO.InstrumentFactory(instrumentName, this));
-    this.set('title', this.attributes.instrument.title);
-    //if audio or midi
-    if (instrumentName === 'Audio File'){
-      this.set('type', 'Audio');
-    } else {
-      this.set('type', 'MIDI');
-      WO.instrumentKeyHandler.create(this.get('instrument'));
+    this.set('title', instrumentName);
+    switch(instrumentName){
+      case 'Audio File':
+        this.set('type', 'Audio');
+        this.set('notes', []);
+        $('.active-track .track-notes').html('');
+        this.set('instrument', WO.InstrumentFactory(instrumentName, this));
+        break;
+      case 'Microphone':
+        this.set('type', 'Microphone');
+        this.set('notes', []);
+        $('.active-track .track-notes').html('');
+        this.set('instrument', WO.InstrumentFactory(instrumentName, this));
+        break;
+      default:
+        var previousInstrumentType = this.get('type');
+        this.set('type', 'MIDI');
+        this.set('instrument', WO.InstrumentFactory(instrumentName, this));
+        WO.instrumentKeyHandler.create(this.get('instrument'));
+
+        if (previousInstrumentType !== 'MIDI') {
+          $('.active-track .track-notes').html('');
+          this.set('mRender', new WO.MidiRender(this.cid + ' .track-notes'));
+        }
+        break;
     }
   }
 });
+
+
+    //see what type of instrumetn current serection is
+      // midi -> mic  =>  remove svg , add mic
+      // midi -> audio  => remove svg , add audio
+      // midi -> midi  => null
+      // mic -> audio  => remove mic , add audio
+      // mic -> midi  =>  remove mike, add svg
+      // audio -> mic  =>  remove audio, add mic
+      // audio -> midi  =>  remove audio,  add svg
+      // keep notes only for midi change to hear different instruments.
