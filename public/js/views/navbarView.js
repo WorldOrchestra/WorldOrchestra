d3333cf8569611f5e517583f5e191d4b3f74b504
David Fedele <dave.fedele@gmail.com>
2015-02-28 18:57:21 -0800

    ajax on ready to load user songs in dropdown

--- public/js/views/navbarView.js
+++ public/js/views/navbarView.js
@@ -2,6 +2,20 @@ WO = WO || {};
 WO.navbarView = Backbone.View.extend({
 
   initialize: function() {
+    $(document).ready(function(){
+      $.ajax({
+        type: "GET",
+        url: window.location + "api/songs",
+      })
+        .done(function(data){
+          $("#user-song-dropdown-list").html("");
+          $("#user-song-dropdown-list").append("<option >My Songs</option>");
+          for( var key in data){
+            $("#user-song-dropdown-list").append("<option value='" + data[key].title +"'>" + data[key].title + "</option>");
+            console.log(data[key].title);
+          }
+        });
+    });
   },
 
   events: {
@@ -12,10 +26,16 @@ WO.navbarView = Backbone.View.extend({
 
       '<div id="navbar">'+
       '<ul>'+
-         '<li class="active"><a href="index.html"><span>WorldOrchestra</span></a></li>'+
-         '<li><a href="#"><span>Songs</span></a></li>'+
-         '<li><a href="#"><span>Sign Up</span></a></li>'+
-         '<li class="last"><a href="#"><span>Login</span></a></li>'+
+        '<li class="active"><a href="index.html"><span>WorldOrchestra</span></a></li>'+
+        '<li><a href="#"><span>Songs</span></a></li>'+
+        '<li><a href="#"><span>Sign Up</span></a></li>'+
+        '<li class="last"><a href="#"><span>Login</span></a></li>'+
+        '<li>'+
+          '<div id=userSongDropdown">'+
+            '<select id="user-song-dropdown-list">'+
+            '</select>'+
+          '</div>'+
+        '</li>'+
       '</ul>'+
       '</div>'
   ),
