730499eae4f50aee384758e9aafb8f103095db7e
David Fedele <dave.fedele@gmail.com>
2015-03-06 17:57:55 -0800

    update env variables

--- server/config.js
+++ server/config.js
@@ -1,9 +1,13 @@
-// 'use strict';
+'use strict';
 
 var mongoose = require('mongoose');
 
 var localDevPath = 'mongodb://localhost:27017/worldOrchestra-dev';
-var dbPath = process.env.MONGODB_URI || localDevPath;
+var dbPath =  process.env.MONGOLAB_URI ||
+              process.env.MONGOHQ_URL ||
+              process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
+              process.env.MONGODB_URI || 
+              localDevPath;
 
 mongoose.connect(dbPath);
 
