e6b82e31d3d684560cdf6f19b071592ff062c3eb
David Fedele <dave.fedele@gmail.com>
2015-03-07 15:44:29 -0800

    update deprecated calls

--- server/api/track/track.controller.js
+++ server/api/track/track.controller.js
@@ -74,7 +74,7 @@ exports.update = function(req, res) {
       if (err) {
         return handleError(res, err);
       }
-      return res.json(200, track);
+      return res.status(200).json(track);
     });
   });
 };
